1d709f9e7b3b597ccaae218428feccde65110f57
reSSSno <46084217+reSSSno@users.noreply.github.com>
2025-10-18 16:07:46 +0300

    Update mvp_dating_service.py

--- mvp_dating_service.py
+++ mvp_dating_service.py
@@ -3,16 +3,23 @@ res = sorted(boys)
 girls = ["Kate", "Liza", "Kira", "Emma", "Trisha"] 
 res2 = sorted(girls)
 
-i = 0
-perfect_para = []
+
 print("#"*15,"\n","Идеальная пара:")
 for n1, n2 in zip(res, res2):
     print(n1 + " and " + n2)
 print("#"*15,"\n")
 
 boys = ["Peter", "Alex", "John", "Arthur", "Richard", "Michael"] 
-res = sorted(boys)
+res3 = sorted(boys)
 girls = ["Kate", "Liza", "Kira", "Emma", "Trisha"] 
-res2 = sorted(girls)
-print(res)
-print(res2)
+res4 = sorted(girls)
+
+if len(res3) > len(res4):
+    print("Внимание, кто-то может остаться без пары!")
+elif len(res4) > len(res3):
+    print("Внимание, кто-то может остаться без пары!")
+else:
+    print("#"*15,"\n","Идеальная пара:")
+    for n1, n2 in zip(res3, res4):
+        print(n1 + " and " + n2)
+    print("#"*15,"\n")
