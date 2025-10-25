9b80765bd73b47ac4a9eba53bb30596c89e731c6
reSSSno <46084217+reSSSno@users.noreply.github.com>
2025-10-25 17:38:57 +0300

    Create purchases_from_purchase_log.py

---
+++ purchases_from_purchase_log.py
@@ -0,0 +1,26 @@
+import json
+
+i = 0
+purchases = {}
+with open('/content/sample_data/purchase_log.txt') as f:
+  for line in f:
+    slovar = json.loads(line)
+    #print(slovar.get('user_id')+" "+slovar.get('category'))
+    key1 = slovar.get('user_id')
+    key2 = slovar.get('category')
+    purchases[key1] = key2
+    i += 1
+ 
+del(purchases['user_id'])
+#print(purchases)
+
+#Проверка
+n = 0
+for key, value in purchases.items():
+  print(key, value)
+  n += 1
+  if n > 1:
+    break
+
+
+#Проверить можно в google colab
